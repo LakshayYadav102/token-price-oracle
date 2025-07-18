148b708fcbf7cf30bf3eda9c86591cbe12912410
LakshayYadav102 <lakshay22csu102@ncuindia.edu>
2025-07-18 12:16:48 +0530

    minor changes

--- client/src/app/utils/api.js
+++ client/src/app/utils/api.js
@@ -1,11 +1,8 @@
+// src/utils/api.ts
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_BASE ||
-    (typeof window !== 'undefined' && window.location.hostname.includes('localhost')
-      ? 'http://localhost:5000/api'
-      : 'https://token-price-api.onrender.com/api'),
+  baseURL: process.env.NEXT_PUBLIC_API_BASE,
   headers: {
     'Content-Type': 'application/json',
   },
